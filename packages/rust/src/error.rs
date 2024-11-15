6f71982a6a8f82b3501df92db31f220f757074cb
YaroShkvorets <shkvorets@gmail.com>
2024-11-15 14:58:09 -0500

    use thiserror

--- packages/rust/src/error.rs
+++ packages/rust/src/error.rs
@@ -1,51 +1,15 @@
 use std::io;
+use thiserror::Error;
 
-// Add this error type at the top of the file
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    Io(io::Error),
-    Parse(serde_json::Error),
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("Parse error: {0}")]
+    Parse(#[from] serde_json::Error),
+
     #[cfg(feature = "fetch")]
-    Http(reqwest::Error),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::Io(e) => write!(f, "IO error: {}", e),
-            Error::Parse(e) => write!(f, "Parse error: {}", e),
-            #[cfg(feature = "fetch")]
-            Error::Http(e) => write!(f, "HTTP error: {}", e),
-        }
-    }
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Error::Io(e) => Some(e),
-            Error::Parse(e) => Some(e),
-            #[cfg(feature = "fetch")]
-            Error::Http(e) => Some(e),
-        }
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Error::Io(err)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::Parse(err)
-    }
-}
-
-#[cfg(feature = "fetch")]
-impl From<reqwest::Error> for Error {
-    fn from(err: reqwest::Error) -> Self {
-        Error::Http(err)
-    }
+    #[error("HTTP error: {0}")]
+    Http(#[from] reqwest::Error),
 }
